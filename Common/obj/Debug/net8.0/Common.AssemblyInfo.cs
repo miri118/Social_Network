ecce1619421f05cf8dc8a623b93b5a0c66dc1977
Brachi-Izkovich <0264brachi@gmail.com>
2025-05-11 02:03:39 +0300

    commit

--- Common/obj/Debug/net8.0/Common.AssemblyInfo.cs
+++ Common/obj/Debug/net8.0/Common.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7030a4a46281cfa159162a9f9995a8caf94aa5f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92eeb6d97ee33c624bef0f7ce470ada2ff75046c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
